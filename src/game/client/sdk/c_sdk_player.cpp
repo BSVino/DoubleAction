adc5d608680adf30e733befb8a399963694b6df8
Jorge Rodriguez <bs.vino@gmail.com>
2013-01-02 12:28:47 -0800

    Limit the amount of camera shit so players don't abuse it.

--- src/game/client/sdk/c_sdk_player.cpp
+++ src/game/client/sdk/c_sdk_player.cpp
@@ -1391,9 +1391,9 @@ void C_SDKPlayer::UpdateSoundEvents()
 	}
 }
 
-static ConVar da_cam_back("da_cam_back", "60", FCVAR_USERINFO|FCVAR_ARCHIVE);
-static ConVar da_cam_right("da_cam_right", "15", FCVAR_USERINFO|FCVAR_ARCHIVE);
-static ConVar da_cam_up("da_cam_up", "10", FCVAR_USERINFO|FCVAR_ARCHIVE);
+static ConVar da_cam_back("da_cam_back", "60", FCVAR_USERINFO|FCVAR_ARCHIVE, "How far back from the eye position the third person camera sits", true, 5, true, 150);
+static ConVar da_cam_right("da_cam_right", "15", FCVAR_USERINFO|FCVAR_ARCHIVE, "How far right from the eye position the third person camera sits", true, -30, true, 30);
+static ConVar da_cam_up("da_cam_up", "10", FCVAR_USERINFO|FCVAR_ARCHIVE, "How far up from the eye position the third person camera sits", true, -20, true, 30);
 
 static ConVar dab_aimin_fov_delta_high("dab_aimin_fov_delta_high", "40", FCVAR_CHEAT|FCVAR_DEVELOPMENTONLY);
 static ConVar dab_aimin_fov_delta_low("dab_aimin_fov_delta_low", "10", FCVAR_CHEAT|FCVAR_DEVELOPMENTONLY);
