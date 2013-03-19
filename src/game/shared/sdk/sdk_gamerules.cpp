d8544cb9d307afeee2a77efd0aec8f2e38fcffcb
shmopaloppa <professorblauvelt@gmail.com>
2013-03-19 09:39:55 -0700

    actually for reals fixed fire on spawn, what a silly problem

--- src/game/shared/sdk/sdk_gamerules.cpp
+++ src/game/shared/sdk/sdk_gamerules.cpp
@@ -972,8 +972,6 @@ void CSDKGameRules::PlayerSpawn( CBasePlayer *p )
 			pPlayer->SetModel( SDK_PLAYER_MODEL );
 		}
 #else
-		//need hard sync here or baseclass weapon behavior causes problems
-		pPlayer->m_flCurrentTime = gpGlobals->curtime;
 		pPlayer->GiveDefaultItems();
 #endif // SDK_USE_PLAYERCLASSES
 		pPlayer->SetMaxSpeed( 600 );
