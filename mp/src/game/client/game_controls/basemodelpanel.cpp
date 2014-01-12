d78e6ec99be901924d38542e2b7875b867b56111
Jorge Rodriguez <bs.vino@gmail.com>
2014-01-12 13:08:50 -0500

    Fix model swapping so that the preview can be empty if desired. (This class has some memory leak problems with the model names but I'm not going to bother fixing them, it's Valve code.)

--- mp/src/game/client/game_controls/basemodelpanel.cpp
+++ mp/src/game/client/game_controls/basemodelpanel.cpp
@@ -217,14 +217,14 @@ void CModelPanel::SetDefaultAnimation( const char *pszName )
 //-----------------------------------------------------------------------------
 void CModelPanel::SwapModel( const char *pszName, const char *pszAttached )
 {
-	if ( !m_pModelInfo || !pszName || !pszName[0] )
-		return;
-
 	int len = Q_strlen( pszName ) + 1;
 	char *pAlloced = new char[ len ];
 	Assert( pAlloced );
 	Q_strncpy( pAlloced, pszName, len );
 	m_pModelInfo->m_pszModelName = pAlloced;
+
+	if ( !m_pModelInfo || !pszName || !pszName[0] )
+		return;
 
 	ClearAttachedModelInfos();
 
