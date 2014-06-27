0cca8030a41debcb3180f98be1ece36185d8fe74
Jorge Rodriguez <jorge@lunarworkshop.com>
2014-06-27 13:30:45 -0700

    30 minute ban for any kicked player.

--- mp/src/game/shared/sdk/sdk_gamerules.cpp
+++ mp/src/game/shared/sdk/sdk_gamerules.cpp
@@ -3112,9 +3112,13 @@ public:
 		{
 			ConVarRef bot_quota("bot_quota");
 			bot_quota.SetValue(bot_quota.GetInt()-1);
+			return;
 		}
 
+		engine->ServerCommand(UTIL_VarArgs( "banid 30 %s\n", GetDetailsString() ));
 		engine->ServerCommand(UTIL_VarArgs( "kickid %s\n", GetDetailsString() ));
+		engine->ServerCommand("writeip\n");
+		engine->ServerCommand("writeid\n");
 	}
 
 	virtual void		ListIssueDetails( CBasePlayer *pForWhom )
