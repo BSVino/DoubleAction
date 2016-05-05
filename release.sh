b15d4d56cedc4e26854c86b5f248b77cc4cc1a8c
TomyLobo <tomylobo@nurfuerspam.de>
2016-05-05 04:41:04 +0200

    Add release script

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+version="$1"
+if [ -z "$version" ]; then
+    echo >&2 'No version given'
+    exit 1
+fi
+
+tag=v"$version"
+
+./changelog.sh --future-release "$tag"
+git commit -m "Release $version" CHANGELOG.md
+git tag "$tag"
+git branch -f master
+
+git push origin "$tag" develop master
