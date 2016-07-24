41593f283e225ce37acdc45b5cf6b39834141bf7
TomyLobo <tomylobo@nurfuerspam.de>
2016-07-24 11:20:48 +0200

    Put the entire script into an if block in order to make bash read it ahead of time

--- release.sh
+++ release.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
+if true; then
+    set -e
 
-set -e
+    version="$1"
+    if [ -z "$version" ]; then
+        echo >&2 'No version given'
+        exit 1
+    fi
 
-version="$1"
-if [ -z "$version" ]; then
-    echo >&2 'No version given'
-    exit 1
+    tag=v"$version"
+
+    git checkout develop
+    git pull -r origin develop
+    ./changelog.sh --future-release "$tag"
+    git commit -m "Release $version" CHANGELOG.md
+    git tag "$tag"
+    git branch -f master
+
+    git push origin "$tag" develop master
 fi
-
-tag=v"$version"
-
-git checkout develop
-git pull -r origin develop
-./changelog.sh --future-release "$tag"
-git commit -m "Release $version" CHANGELOG.md
-git tag "$tag"
-git branch -f master
-
-git push origin "$tag" develop master
