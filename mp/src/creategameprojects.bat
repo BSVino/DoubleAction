fc0844a5d869b387f23e1d04c4f4a1f70ee78f81
TomyLobo <tomylobo@nurfuerspam.de>
2018-09-21 18:42:57 +0200

    Define DA_GIT_REVISION macro as the current Git revision

--- mp/src/creategameprojects.bat
+++ mp/src/creategameprojects.bat
@@ -1,5 +1,6 @@
 @echo off
 pushd %~dp0
+sed -ri '/PLACEHOLDER_COMPILER_ARGS/ s/^.*$/git rev-parse HEAD/e; T; s/^.*$/$PreprocessorDefinitions "$BASE;DA_GIT_VERSION=\0"/' game/client/client_da.vpc game/server/server_da.vpc
 devtools\bin\vpc.exe /da +game /mksln DoubleAction.sln /2010
 popd
 pause
