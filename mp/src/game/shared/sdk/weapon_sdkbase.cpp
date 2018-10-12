aa2dad5dec2c86f2213b1a7b322882c734595ec0
TomyLobo <tomylobo@nurfuerspam.de>
2018-10-12 21:32:55 +0200

    Disable the overflow weapon drop mechanism by default
    Can be re-enabled by setting da_drop_overflow_on_pickup to 1.

--- mp/src/game/shared/sdk/weapon_sdkbase.cpp
+++ mp/src/game/shared/sdk/weapon_sdkbase.cpp
@@ -1559,6 +1559,8 @@ void CWeaponSDKBase::ItemPostFrame( void )
 	}
 }
 
+ConVar da_drop_overflow_on_pickup("da_drop_overflow_on_pickup", "0", FCVAR_ARCHIVE | FCVAR_USERINFO | FCVAR_CLIENTDLL, "Enable to drop weapons in order to make room for what you pick up.");
+
 extern bool UTIL_ItemCanBeTouchedByPlayer( CBaseEntity *pItem, CBasePlayer *pPlayer );
 
 void CWeaponSDKBase::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
@@ -1582,7 +1584,7 @@ void CWeaponSDKBase::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYP
 
 	int iWeight = GetWeight();
 
-	if (iWeight + pPlayer->FindCurrentWeaponsWeight() > MAX_LOADOUT_WEIGHT)
+	if (da_drop_overflow_on_pickup.GetBool() && iWeight + pPlayer->FindCurrentWeaponsWeight() > MAX_LOADOUT_WEIGHT)
 		pPlayer->DropWeaponsToPickUp(this);
 
 	if (iWeight + pPlayer->FindCurrentWeaponsWeight() > MAX_LOADOUT_WEIGHT)
