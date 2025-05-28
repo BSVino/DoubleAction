4d1003dd29800901669263859f7af0e44356ef55
Jorge Rodriguez <jorge@lunarworkshop.com>
2025-05-28 15:49:55 -0700

    The Athletic style skill now gets a +50% style bonus while stunting

--- mp/src/game/server/sdk/sdk_player.cpp
+++ mp/src/game/server/sdk/sdk_player.cpp
@@ -4162,6 +4162,11 @@ void CSDKPlayer::AddStylePoints(float points, style_sound_t eStyle, announcement
 	if (SDKGameRules()->GetBountyPlayer() == this)
 		SDKGameRules()->HealWanted(points);
 
+	if (m_Shared.IsDiving() || m_Shared.IsSliding() || m_Shared.IsRolling() || m_Shared.IsSuperFalling() || m_Shared.IsWallFlipping() || m_Shared.IsManteling())
+	{
+		points = m_Shared.ModifySkillValue(points, 0.5, SKILL_ATHLETIC);
+	}
+
 	points *= GetDKRatio(0.7, 2, true);
 
 	m_flTotalStyle += points;
