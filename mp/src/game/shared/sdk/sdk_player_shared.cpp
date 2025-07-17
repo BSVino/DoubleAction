3c187070b71350b3e4d7b954a152fb278d470947
Jorge Rodriguez <jorge@lunarworkshop.com>
2025-07-17 11:18:32 -0700

    Undoing this not-fix, it was correct before and this stuff is just confusing to think about.

--- mp/src/game/shared/sdk/sdk_player_shared.cpp
+++ mp/src/game/shared/sdk/sdk_player_shared.cpp
@@ -1687,9 +1687,9 @@ float CSDKPlayer::GetSlowMoMultiplier() const
 
 float CSDKPlayer::GetSlowMoGoal() const
 {
-	if (m_iSlowMoType == SLOWMO_ACTIVATED)
+	if (m_iSlowMoType == SLOWMO_STYLESKILL)
 		return 0.7f;
-	else if (m_iSlowMoType == SLOWMO_STYLESKILL)
+	else if (m_iSlowMoType == SLOWMO_ACTIVATED)
 		return 0.65f;
 	else if (m_iSlowMoType == SLOWMO_SUPERFALL)
 		return 0.65f;
