4a3da3ebe9414ae5bab620fdc0fc84feb0501d5d
Jorge Rodriguez <jorge@lunarworkshop.com>
2025-07-19 13:06:49 -0700

    Some scripts to help me use VS13 without having a license, since it's not possible to register it anymore

---
+++ mp/src/compilerelease.bat
@@ -0,0 +1,3 @@
+@echo off
+"c:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" DoubleAction.sln /p:configuration=Release
+pause
---
+++ mp/src/runlocalbuild.bat
@@ -0,0 +1,2 @@
+@echo off
+"c:\Program Files (x86)\Steam\steamapps\common\Double Action\hl2.exe" -game "d:\git\DoubleAction\mp\game\dab" -dev +sv_cheats 1
