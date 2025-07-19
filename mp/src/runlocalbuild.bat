c63640b17bbec61073b97aba3e1c70bf08d71a2c
Jorge Rodriguez <jorge@lunarworkshop.com>
2025-07-19 14:08:14 -0700

    Sublime project files and batch file update

--- mp/src/runlocalbuild.bat
+++ mp/src/runlocalbuild.bat
@@ -1,2 +1,2 @@
 @echo off
-"c:\Program Files (x86)\Steam\steamapps\common\Double Action\hl2.exe" -game "d:\git\DoubleAction\mp\game\dab" -dev +sv_cheats 1
+"c:\Program Files (x86)\Steam\steamapps\common\Double Action\hl2.exe" -game "d:\git\DoubleAction\mp\game\dab" -dev +sv_cheats 1 +map da_megachat
